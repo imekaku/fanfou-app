527094f31c3849cca82765bafd037baa930a5d18
imekaku <imekaku@gmail.com>
2017-10-19 22:50:49 +0800

    app init

---
+++ src/main/java/com/imekaku/fanfou/FanfouApplication.java
@@ -0,0 +1,12 @@
+package com.imekaku.fanfou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanfouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanfouApplication.class, args);
+	}
+}
---
+++ src/test/java/com/imekaku/fanfou/FanfouApplicationTests.java
@@ -0,0 +1,16 @@
+package com.imekaku.fanfou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FanfouApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
